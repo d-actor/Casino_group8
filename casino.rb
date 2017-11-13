8c42a817e878c561a209834272a240a0f50e475d
FuChai Mead <jessie.mf.wolff@gmail.com>
2017-11-13 11:21:03 -0700

    clean up menu options

--- casino.rb
+++ casino.rb
@@ -1,14 +1,32 @@
 require 'pry'
 require 'colorize'
 require_relative 'player'
+require_relative 'high_low'
 
 class Casino
-  attr_accessor :player
-
+  attr_accessor :player, :options
+ 
   def initialize
     puts "Welcome to the Ruby Casino!"
     @player = Player.new
-    binding.pry
+    @options = ["High/Low", "Exit"]
+    menu
+  end
+
+  def menu
+    @options.each_with_index { |opt, i| puts "#{i + 1}) #{opt}" }
+    choice = gets.to_i - 1 
+    case choice
+      when 0 
+        HighLow.new(@player)
+      when 1
+        puts "Come again."
+        exit 
+      else
+        puts "Invalid choice."
+        menu
+      end
+      menu
   end
 end
 
---
+++ high_low.rb
@@ -0,0 +1,8 @@
+
+class HighLow
+  attr_accessor :player 
+  def initialize(player)
+    @player = player
+
+    end
+  end
