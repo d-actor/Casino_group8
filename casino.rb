f1c2c8d4cf2dc4938cb51a0510dcfb13d59299ee
FuChai Mead <jessie.mf.wolff@gmail.com>
2017-11-13 10:10:35 -0700

    add player class

--- casino.rb
+++ casino.rb
@@ -1,13 +1,13 @@
 require 'pry'
 require 'colorize'
+require_relative 'player'
 
 class Casino
+  attr_accessor :player
+  
   def initialize
     puts "Welcome to the Ruby Casino!"
-    puts "What is your name, player?"
-    name = gets.strip
-    # create a new player instance and probably get the name
-    # in the players initialize
+    @player = Player.new
   end
 end
 
