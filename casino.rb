4fdd099668858a1dff6c006a315315a19b703438
Dan Actor <danltactor@gmail.com>
2017-11-13 10:21:24 -0700

    add wallet class

--- casino.rb
+++ casino.rb
@@ -4,10 +4,11 @@ require_relative 'player'
 
 class Casino
   attr_accessor :player
-  
+
   def initialize
     puts "Welcome to the Ruby Casino!"
     @player = Player.new
+    binding.pry
   end
 end
 
--- player.rb
+++ player.rb
@@ -1,6 +1,9 @@
+require_relative 'wallet'
+
 class Player
   MIN_AGE = 21
-  attr_accessor :name, :age 
+  
+  attr_accessor :name, :age, :wallet
 
   def initialize
     puts "What is your name, player?"
@@ -8,15 +11,17 @@ class Player
     puts "What is your age?"
     @age = gets.to_i
     age_verification
+    puts 'How much money are you playing with?'
+    amount = gets.to_f
+    @wallet = Wallet.new(amount)
   end
 
   def age_verification
     if @age < MIN_AGE
-      # `say GET OUT OF HERE!` 
+      # `say GET OUT OF HERE!`
       puts "GET OUT OF HERE!"
       exit
     end
   end
 
 end
-  
