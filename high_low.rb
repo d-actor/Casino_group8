31b25f8fab854b61019ad1aea6a89e36a679850a
Uli12 <31193167+Uli12@users.noreply.github.com>
2017-11-13 12:17:42 -0700

    Make changes to HighLow.

--- high_low.rb
+++ high_low.rb
@@ -1,8 +1,34 @@
+require_relative 'player'
+
+@house_number = *(1..100)
 
 class HighLow
   attr_accessor :player 
   def initialize(player)
     @player = player
+  end
+
+  def game
+    puts "Please choose a number between 1 and 100."
+    gets.to_i
+    puts "Casino: I am going to place a concealed number between 1 and 100."
+    puts 
+    house_number = @house_number.sample
+    puts "Please place a bet."
+    bet = gets.strip.to_i
+    puts "Casino: Is the number I placed higher of lower than your number."
 
     end
+   
   end
+
+
+player = Player.new
+HighLow.new(player)
+
+
+
+  #have player choose a number between 1 and 100
+  #give a number
+  #have player guess if the number you presented is high or low
+  #if correct win, if incorrect lose
