5adc0833b452305a8986682ce35f49efa32c33e7
FuChai Mead <jessie.mf.wolff@gmail.com>
2017-11-13 21:52:12 -0700

    final slot machine with symbols

--- slotmachine.rb
+++ slotmachine.rb
@@ -8,6 +8,7 @@ class SlotMachine
   def initialize(player)
     @player = player
   puts "You are about to play the slot machine #{player.name}.".yellow
+  puts "This machine only accepts fives."
   menu
   end
 
@@ -28,36 +29,68 @@ end
 
 def place_bet
   puts "How much do you want to bet? ($#{@player.wallet.amount})".yellow
-  print `clear`
   @bet = gets.to_f
-  if @bet > 0 && @bet < @player.wallet.amount 
-  @player.wallet.amount -= @bet
-  puts "You have $#{@player.wallet.amount} left.".yellow
-  play
-  else 
+  if @bet == 5
+    if @bet < @player.wallet.amount
+      @player.wallet.amount -= @bet
+    puts "You have $#{@player.wallet.amount} left.".yellow
+    play
+     else 
     puts "You don't have enough to bet that much.".yellow
-    place_bet
+      menu
+     end
+    else
+     puts "That's not a valid bet. This machine only accepts fives."
+     menu
   end
 end
 
+
 def play
-  @arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  @arr2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  @arr3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  @arr1 = ["\u{1F34a}", "\u{1F34b}", "\u{1F34c}", "\u{1F349}",  "\u{1F34d}"]
+  @arr2 = ["\u{1F34a}", "\u{1F34b}", "\u{1F34c}", "\u{1F349}", "\u{1F34d}" ]
+  @arr3 = ["\u{1F34a}", "\u{1F34b}", "\u{1F34c}", "\u{1F349}", "\u{1F34d}" ]
+  @arr4 = ["\u{1F34a}", "\u{1F34b}", "\u{1F34c}", "\u{1F349}",  "\u{1F34d}"]
+  @arr5 = ["\u{1F34a}", "\u{1F34b}", "\u{1F34c}", "\u{1F349}", "\u{1F34d}" ]
+  @arr6 = ["\u{1F34a}", "\u{1F34b}", "\u{1F34c}", "\u{1F349}", "\u{1F34d}" ]
+  @arr7 = ["\u{1F34a}", "\u{1F34b}", "\u{1F34c}", "\u{1F349}",  "\u{1F34d}"]
+  @arr8 = ["\u{1F34a}", "\u{1F34b}", "\u{1F34c}", "\u{1F349}", "\u{1F34d}" ]
+  @arr9 = ["\u{1F34a}", "\u{1F34b}", "\u{1F34c}", "\u{1F349}", "\u{1F34d}" ]
+
+  
 
   number1 = @arr1.sample
   number2 = @arr2.sample
   number3 = @arr3.sample
+  number4 = @arr1.sample
+  number5 = @arr2.sample
+  number6 = @arr3.sample
+  number7 = @arr1.sample
+  number8 = @arr2.sample
+  number9 = @arr3.sample
 
-  
-  puts "You got: #{number1} #{number2} #{number3}."
-  if number1 == number2 && number2 == number3 
-    puts "You Win!!".green
+  puts "You got:"
+  print "#{number1} |" ; sleep (1)
+  print " #{number2} |"; sleep (1)
+  print " #{number3}" ; sleep (1)
+  puts ""
+  print "#{number4} |" ; sleep (1)
+  print " #{number5} |"; sleep (1)
+  print " #{number6}" ; sleep (1)
+  puts ""
+  print "#{number7} |" ; sleep (1)
+  print " #{number8} |"; sleep (1)
+  print " #{number9}" ; sleep (1)
+  puts ""
+
+  if number4 == number5 && number5 == number6 
     `say you win`
+    puts "You Win!!".green
     @player.wallet.amount += @bet*3
     puts "You won $#{@bet*3}. You now have $#{@player.wallet.amount}."
     menu
-    elsif number1 == number2 || number2 == number3
+    elsif number1 == number5 && number5 == number9
+    `say you win`
     puts "You Win!!".green
     @player.wallet.amount += @bet*2
     puts "You won $#{@bet}. You now have $#{@player.wallet.amount}."
@@ -68,4 +101,5 @@ def play
     menu
   end
 end
+
 
